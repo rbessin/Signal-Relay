958369d35b316103a26dcb0fb3e6fd1dca76993c
Raphael Bessin <bessin.raphael@gmail.com>
2025-10-27 14:07:45 -0400

    added wire placement

--- scripts/core/wire.gd
+++ scripts/core/wire.gd
@@ -7,6 +7,8 @@ var line: Line2D
 var color: Color = Color.BLACK
 var width: int = 2
 
+var preview_end_position: Vector2 = Vector2.ZERO
+var is_preview: bool = false
 
 # Runs on instantiation
 func _ready():
@@ -22,7 +24,14 @@ func _process(_delta):
 
 # Set visuals
 func update_visuals():
-	if from_pin == null or to_pin == null: return
+	if from_pin == null: return
 	var start_position = to_local(from_pin.global_position)
-	var end_position = to_local(to_pin.global_position)
+	var end_position: Vector2
+
+	if is_preview:
+		end_position = to_local(preview_end_position)
+	elif to_pin != null:
+		end_position = to_local(to_pin.global_position)
+	else: return
+
 	line.points = [start_position, end_position]
--- scripts/main.gd
+++ scripts/main.gd
@@ -27,21 +27,19 @@ var is_creating_wire: bool = false
 var wire_start_pin: Pin = null
 var wire_preview: Wire = null
 
+# Default functions which run on instantiation and every frame
+func _ready():
+	pass
 func _process(_delta):
+	_drag()
+	_position_wire_preview()
+
+# INTERACT helpers
+func _drag(): # Drag gate instance
 	if is_dragging and selected_gate_instance != null:
-		selected_gate_instance.global_position = get_global_mouse_position() + drag_offset 
+		selected_gate_instance.global_position = get_global_mouse_position() + drag_offset
 
-func instantiate_gate():
-	if gate_to_place == null: return
-	
-	var new_gate = gate_to_place.instantiate()
-	add_child(new_gate)
-	new_gate.uid = _generate_uid()
-	new_gate.name = new_gate.type + '_' + str(new_gate.uid)
-	new_gate.gate_clicked.connect(_select_gate_instance)
-	new_gate.global_position = get_global_mouse_position()
-
-func _select_gate_instance(gate_instance: Gate):
+func _select_gate_instance(gate_instance: Gate): # Select gate instance
 	if current_mode == Mode.INTERACT:
 		if selected_gate_instance != null:
 			selected_gate_instance.set_selected(false)
@@ -50,51 +48,110 @@ func _select_gate_instance(gate_instance: Gate):
 		is_dragging = true
 		drag_offset = selected_gate_instance.global_position - get_global_mouse_position()
 
-func delete_gate_instance():
+func delete_gate_instance(): # Delete gate instance
 	selected_gate_instance.queue_free()
 	selected_gate_instance = null
 
-func _generate_uid():
+# PLACE helpers
+func instantiate_gate(): # Create gate instance
+	if gate_to_place == null: return
+	
+	var new_gate = gate_to_place.instantiate()
+	add_child(new_gate)
+	new_gate.uid = _generate_uid()
+	new_gate.name = new_gate.type + '_' + str(new_gate.uid)
+	new_gate.global_position = get_global_mouse_position()
+	new_gate.gate_clicked.connect(_select_gate_instance)
+	call_deferred("_connect_gate_pins", new_gate)
+
+func _connect_gate_pins(gate: Gate):
+	for child in gate.get_children():
+		if child is Pin:
+			child.pin_clicked.connect(_on_pin_clicked)
+
+func _on_pin_clicked(pin_instance: Pin): # Handle pin clicks
+	if current_mode == Mode.WIRE:
+		if not is_creating_wire:
+			if pin_instance.pin_type == Pin.PinType.OUTPUT:
+				_start_wire_creation(pin_instance)
+		else:
+			if pin_instance.pin_type == Pin.PinType.INPUT:
+				_complete_wire_creation(pin_instance)
+			else: _cancel_wire_creation()
+
+func _generate_uid(): # Create unique ID
 	current_uid += 1
 	return current_uid
 
-func _unhandled_input(event):
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		if event.pressed:
-			if current_mode == Mode.PLACE:
-				instantiate_gate()
-			elif current_mode == Mode.INTERACT:
-				if selected_gate_instance != null:
-					selected_gate_instance.set_selected(false)
-					selected_gate_instance = null
-		if not event.pressed:
-			is_dragging = false
-	if event is InputEventKey:
-		if (event.keycode == KEY_BACKSPACE or event.keycode == KEY_DELETE) and event.pressed:
-			if current_mode == Mode.INTERACT:
-				delete_gate_instance()
+# WIRE helpers
+func _start_wire_creation(start_pin: Pin):
+	is_creating_wire = true
+	wire_start_pin = start_pin
+	wire_preview = Wire.new()
+	wire_preview.from_pin = wire_start_pin
+	wire_preview.is_preview = true
+	add_child(wire_preview)
 
-# Enter / Exit / Select Modes
+func _complete_wire_creation(end_pin: Pin):
+	wire_preview.to_pin = end_pin
+	wire_preview.is_preview = false
+	is_creating_wire = false
+	wire_start_pin = null
+	wire_preview = null
 
+func _cancel_wire_creation():
+	if wire_preview != null:
+		wire_preview.queue_free()
+	is_creating_wire = false
+	wire_start_pin = null
+	wire_preview = null
+
+func _position_wire_preview():
+	if is_creating_wire and wire_preview != null:
+		wire_preview.preview_end_position = get_global_mouse_position()
+
+# Handle events
+func _unhandled_input(event): # Handle inputs
+	if event.is_action_pressed("Click"):
+		_handle_click()
+	elif event.is_action_released("Click"):
+		_handle_click_release()
+	elif event.is_action_pressed("Delete"):
+		_handle_delete()
+	elif event.is_action_pressed("Stop"):
+		_handle_stop()
+
+func _handle_click(): # Handle click
+	if current_mode == Mode.PLACE: instantiate_gate()
+	elif current_mode == Mode.INTERACT:
+		if selected_gate_instance != null:
+			selected_gate_instance.set_selected(false)
+			selected_gate_instance = null
+func _handle_click_release(): # Handle click release
+	is_dragging = false
+func _handle_delete(): # Handle delete
+	if current_mode == Mode.INTERACT: delete_gate_instance()
+func _handle_stop(): # Handle stop
+	if current_mode == Mode.WIRE: _cancel_wire_creation()
+	elif current_mode == Mode.INTERACT: selected_gate_instance = null
+
+# Enter mode
 func _enter_interact():
 	pass
-
 func _enter_place(gate_name: String = ""):
 	if gate_name != "" and gate_name in gate_prefabs:
 		gate_to_place = gate_prefabs[gate_name]
-
 func _enter_wire():
 	pass
 
+# Exit mode
 func _exit_interact():
 	if selected_gate_instance != null:
 		selected_gate_instance.set_selected(false)
 		selected_gate_instance = null
 	is_dragging = false
-
 func _exit_place():
 	gate_to_place = null
-
 func _exit_wire():
 	if wire_preview != null:
 		wire_preview.queue_free()
@@ -102,6 +159,7 @@ func _exit_wire():
 	is_creating_wire = false
 	wire_start_pin = null
 
+# Set mode by exiting old mode and entering new mode
 func _set_mode(new_mode: Mode, gate_name: String = ''):
 	match current_mode:
 		Mode.INTERACT: _exit_interact()
@@ -116,11 +174,10 @@ func _set_mode(new_mode: Mode, gate_name: String = ''):
 		Mode.PLACE: _enter_place(gate_name)
 		Mode.WIRE: _enter_wire()
 
+# Select mode
 func select_interact():
 	_set_mode(Mode.INTERACT)
-
 func select_place(gate_name: String):
 	_set_mode(Mode.PLACE, gate_name)
-
 func select_wire():
 	_set_mode(Mode.WIRE)
