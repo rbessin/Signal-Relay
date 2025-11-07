423aec6f3ab79dc7cf8b4480a9cd33b816e5669b
Raphael Bessin <bessin.raphael@gmail.com>
2025-11-07 16:15:55 -0500

    refactored code on gate.gd and added comments

--- scripts/core/gate.gd
+++ scripts/core/gate.gd
@@ -30,33 +30,29 @@ var collision_shape_2d: CollisionShape2D
 var rectangle_shape_2d: RectangleShape2D
 signal gate_clicked(gate_instance)
 
-# Sets visuals and collisions when added to the scene
-func _ready() -> void:
+# Functions on mount and every frame
+func _ready() -> void: # Sets visuals, collisions and pins when added to the scene
 	set_visuals()
 	set_collisions()
 	create_pins()
 
-# Get input states to set output state
-func evaluate() -> void:
+# Functions to handle evaluation and propagation
+func evaluate() -> void: # Evaluates inputs to set output (to be overriden by subclasses)
 	pass
 
-func evaluate_with_propagation():
+func evaluate_with_propagation() -> void: # Gets inputs, evaluates inputs, sets output (evaluation cycle)
 	var old_output = output_value
 	read_inputs_from_pins()
 	evaluate()
 	write_output_to_pin()
-	if output_value != old_output:
-		propagate_to_wires()
-	print(type, " evaluating, output: ", output_value)
+	if output_value != old_output: propagate_to_wires()
 
-# Set input state
-func set_input(index, value):
-	input_values[index] = value
-	evaluate()
-
-# Get output state
-func get_output():
-	return output_value
+func propagate_to_wires(): # Propagate output to wires
+	for child in get_children():
+		if child is Pin:
+			if child.pin_type == Pin.PinType.OUTPUT:
+				for wire in child.connected_wires:
+					wire.propagate()
 
 # Set visuals
 func set_visuals() -> void:
@@ -89,31 +85,59 @@ func set_visuals() -> void:
 
 # Set collisions
 func set_collisions() -> void:
+	# Create area 2d
 	area_2d = Area2D.new()
 	area_2d.input_pickable = true
-	collision_shape_2d = CollisionShape2D.new()
-	rectangle_shape_2d = RectangleShape2D.new()
-	rectangle_shape_2d.size = size
-	collision_shape_2d.shape = rectangle_shape_2d
-	collision_shape_2d.position = Vector2.ZERO
 	add_child(area_2d)
-	area_2d.add_child(collision_shape_2d)
 	area_2d.input_event.connect(_on_area_input_event)
 
-# Set selection
-func set_selected(select: bool) -> void:
+	# Create rectangle shape 2d
+	rectangle_shape_2d = RectangleShape2D.new()
+	rectangle_shape_2d.size = size
+
+	# Create collision shape 2d
+	collision_shape_2d = CollisionShape2D.new()
+	collision_shape_2d.shape = rectangle_shape_2d
+	collision_shape_2d.position = Vector2.ZERO
+	area_2d.add_child(collision_shape_2d)
+
+# Functions to handle gate
+func set_selected(select: bool) -> void: # Set selection
 	selected = select
 	if selected: border_rect.visible = true
 	else: border_rect.visible = false
 
-# On area input event
-func _on_area_input_event(_viewport, event, _shape_idx) -> void:
+func _on_area_input_event(_viewport, event, _shape_idx) -> void: # Detect click on area 2d
 	if event is InputEventMouseButton:
 		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 			gate_clicked.emit(self)
 
-# Create pins
-func create_pins():
+# Functions to handle inputs and outputs
+func set_input(index, value) -> void: # Set input state
+	input_values[index] = value
+	evaluate()
+
+func get_output() -> bool: # Get output state
+	return output_value
+
+func read_inputs_from_pins() -> void: # Read inputs from pins
+	var input_num: int = 0
+	for child in get_children():
+		if child is Pin:
+			if child.pin_type == Pin.PinType.INPUT:
+				input_values[input_num] = child.signal_state
+				input_num += 1
+
+func write_output_to_pin() -> void: # Write output to pin
+	for child in get_children():
+		if child is Pin:
+			if child.pin_type == Pin.PinType.OUTPUT:
+				child.signal_state = output_value
+				child.update_visuals()
+				return
+
+# Functions to handle pins
+func create_pins(): # Create pins
 	for i in range(num_inputs): # Create input pins
 		var pin = Pin.new()
 		pin.pin_type = Pin.PinType.INPUT
@@ -132,47 +156,18 @@ func create_pins():
 		pin.position = Vector2(x_pos, y_pos)
 		add_child(pin)
 
-# Get pin index with instance
-func get_pin_by_index(pin_type: Pin.PinType, index: int) -> Pin:
+func get_pin_by_index(pin_type: Pin.PinType, index: int) -> Pin: # Get pin with pin index (loading)
 	var counter = 0
 	for child in get_children():
 		if child is Pin and child.pin_type == pin_type:
-			if counter == index:
-				return child
+			if counter == index: return child
 			counter += 1
 	return null
-# Get pin index with instance (for saving)
-func get_pin_index(pin: Pin) -> int:
+
+func get_pin_index(pin: Pin) -> int: # Get pin index with instance (saving)
 	var counter = 0
 	for child in get_children():
 		if child is Pin and child.pin_type == pin.pin_type:
-			if child == pin:
-				return counter
+			if child == pin: return counter
 			counter += 1
 	return -1
-
-# Read inputs
-func read_inputs_from_pins():
-	var input_num: int = 0
-	for child in get_children():
-		if child is Pin:
-			if child.pin_type == Pin.PinType.INPUT:
-				input_values[input_num] = child.signal_state
-				input_num += 1
-
-# Write output
-func write_output_to_pin():
-	for child in get_children():
-		if child is Pin:
-			if child.pin_type == Pin.PinType.OUTPUT:
-				child.signal_state = output_value
-				child.update_visuals()
-				return
-
-# Propagate output
-func propagate_to_wires():
-	for child in get_children():
-		if child is Pin:
-			if child.pin_type == Pin.PinType.OUTPUT:
-				for wire in child.connected_wires:
-					wire.propagate()
