e998386c1a3d989243165a4900a14dd24cf69fd5
Raphael Bessin <bessin.raphael@gmail.com>
2025-11-07 16:34:42 -0500

    added comments to circuit_serializer.gd

--- scripts/core/circuit_serializer.gd
+++ scripts/core/circuit_serializer.gd
@@ -1,11 +1,15 @@
 class_name CircuitSerializer
 
+# Saves circuit to JSON file [Arrays -> JSON]
 static func save_to_json(gates_array: Array, wires_array: Array, circuit_name: String) -> bool:
+	# Define dictionary structure
 	var saving_dictionary: Dictionary = {
 		"circuit_name": circuit_name,
 		"gates": [],
 		"wires": []
 	}
+
+	# Add gate instances to dictionary
 	for gate in gates_array:
 		var gate_info: Dictionary = {}
 		gate_info["uid"] = gate.uid
@@ -13,7 +17,8 @@ static func save_to_json(gates_array: Array, wires_array: Array, circuit_name: S
 		gate_info["x"] = gate.position.x
 		gate_info["y"] = gate.position.y
 		saving_dictionary["gates"].append(gate_info)
-		
+	
+	# Add wire instances to dictionary
 	for wire in wires_array:
 		var wire_info: Dictionary = {}
 		wire_info["from_gate"] = wire.from_pin.parent_gate.uid
@@ -22,48 +27,59 @@ static func save_to_json(gates_array: Array, wires_array: Array, circuit_name: S
 		wire_info["to_pin"] = wire.to_pin.parent_gate.get_pin_index(wire.to_pin)
 		saving_dictionary["wires"].append(wire_info)
 	
+	# Convert dictionary to JSON string
 	var json_string: String = JSON.stringify(saving_dictionary, "\t")
 
+	# Handle folder existance
 	var dir = DirAccess.open("user://")
 	if not dir:
 		print("ERROR: Could not open user directory.")
 		return false
 	
+	# Handle subfolder existance
 	if not dir.dir_exists("circuits"):
 		var error = dir.make_dir("circuits")
 		if error != OK:
 			print("ERROR: Could not create circuits directory.")
 			return false
 
+	# Safely convert name and define filepath
 	var safe_name: String = circuit_name.replace("/", "_")
 	var filepath = "user://circuits/" + safe_name + ".json"
 
+	# Open JSON file and handle opening errors
 	var file = FileAccess.open(filepath, FileAccess.WRITE)
 	if not file:
 		print("ERROR: Could not open file for writing: ", filepath)
 		return false
 	
+	# Store JSON string in JSON file
 	file.store_string(json_string)
-	print("SUCCESS: Circuit saved to ", filepath)
 	
 	return true
 
+# Loads circuit from JSON file [JSON -> Dictionary]
 static func load_from_json(circuit_name: String):
+	# Safely convert name and define filepath
 	var safe_name: String = circuit_name.replace("/", "_")
 	var filepath = "user://circuits/" + safe_name + ".json"
 	
+	# Open JSON file and handle opening errors
 	var file = FileAccess.open(filepath, FileAccess.READ)
 	if not file:
 		print("ERROR: Could not open file for reading: ", filepath)
 		return null
 	
+	# Read JSON file -> string -> dictionary
 	var json_string = file.get_as_text()
 	var json_dict = JSON.parse_string(json_string)
 
+	# Handle wrong filepath
 	if not json_dict:
 		print("ERROR: Could not read file: ", filepath)
 		return null
 	
+	# Handle missing information (name, gates, wires)
 	if not json_dict.has("circuit_name"):
 		print("ERROR: File does not include a circuit name.")
 		return null
